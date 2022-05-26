addfb41be193064a5606b4cbd87c0b3a05c85c7e
janinquisitor <jancarlos0195@gmail.com>
2022-05-26 15:34:50 -0400

    added authentication

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,15 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from models import Post
+from utils.auth import login_required
+from db import get_db
 
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+
 
 # This time, using the url_prefix argument, we prefix every route in the blueprint with /dashboard.
 # The routes thus become /dashboard and /dashboard/edit/<id> when registered with the app.
 
 @bp.route('/')
+@login_required
 def dash():
-    return render_template('dashboard.html')
+    db = get_db()
+    posts = db.query(Post).filter(Post.user_id == session.get('user_id')).order_by(Post.created_at.desc()).all()
+
+    return render_template(
+        'dashboard.html',
+        posts=posts,
+        loggedIn=session.get('loggedIn')
+    )
 
 
 @bp.route('/edit/<id>')
+@login_required
 def edit(id):
-    return render_template('edit-post.html')
+    # get single post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
+    # render edit page
+    return render_template(
+        'edit-post.html',
+        post=post,
+        loggedIn=session.get('loggedIn')
+    )
